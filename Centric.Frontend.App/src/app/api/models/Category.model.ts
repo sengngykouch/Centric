9415f2442a5d45c6a00959e6bd26fa05d572bffb
Sengngy Kouch <kouchsengngy@gmail.com>
2025-07-30 19:36:16 -0400

    Add example for how to fetch data from the backend.

---
+++ Centric.Frontend.App/src/app/api/models/Category.model.ts
@@ -0,0 +1,6 @@
+export type Category = {
+    readonly categoryId: number;
+    readonly name: string;
+    readonly isActive: boolean;
+    readonly userId: number;
+};
--- Centric.Frontend.App/src/app/area/home/home.component.ts
+++ Centric.Frontend.App/src/app/area/home/home.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+
+import { Category } from '../../api/models/Category.model';
 
 @Component({
     selector: 'app-home',
-    imports: [],
+    imports: [MatButtonModule, CommonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+    private _categories: Category[] = [];
+
+    protected get categories() {
+        return this._categories;
+    }
+
+    protected async onGetCategories(): Promise<void> {
+
+        const baseUrl = 'http://localhost:5020/api/';
+
+        const response = await fetch(baseUrl + 'category', { method: 'GET' });
+
+        if (!response.ok)
+            throw new Error('Failed to fetch');
+
+        this._categories = await response.json();
+    }
 }
